0997f59701456d9fa21e3e0a0685910ddd355da5
Ben Yellin <byellindc@gmail.com>
2019-05-11 19:41:00 -0400

    add word math processor

--- src/processors/index.js
+++ src/processors/index.js
@@ -1,7 +1,9 @@
 import CommentProcessor from './comment_processor';
 import VariableAssignmentProcessor from './variable_assignment_processor';
+import WordMathProcessor from './word_math_processor';
 
 export default [
   CommentProcessor,
-  VariableAssignmentProcessor
+  VariableAssignmentProcessor,
+  WordMathProcessor
 ];
