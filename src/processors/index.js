d58b9fd3acf4a6c6e13442d8df5c78b34a398bdf
Ben Yellin <byellindc@gmail.com>
2019-05-11 19:43:54 -0400

    add percentage processor

--- src/processors/index.js
+++ src/processors/index.js
@@ -1,9 +1,11 @@
 import CommentProcessor from './comment_processor';
 import VariableAssignmentProcessor from './variable_assignment_processor';
 import WordMathProcessor from './word_math_processor';
+import PercentageProcessor from './percentage_processor';
 
 export default [
   CommentProcessor,
   VariableAssignmentProcessor,
-  WordMathProcessor
+  WordMathProcessor,
+  PercentageProcessor
 ];
---
+++ src/processors/percentage_processor.js
@@ -0,0 +1,37 @@
+import Processor from '../processor';
+
+export default class PercentageProcessor extends Processor {
+  constructor() {
+    super(
+      'percentage',
+      {
+        match: /(?<percentage>-?[\d.]+)% (?<operator>off?|on) (?<expr>.*$)/,
+        reserved: ['of', 'off', 'on'],
+        priority: 2
+      },
+      (matches) => {
+        const percentage = parseFloat(matches[1]);
+        const operator = matches[2];
+        const line_expr = matches[3];
+        const expr = `${line_expr} * ${percentage/100}`;
+  
+        let output;
+        switch (operator) {
+          case 'of':
+            output = expr;
+            break;
+          case 'off':
+            output = `${expr} - (${expr})`;
+            break;
+          case 'on':
+            output = `${expr} + (${expr})`;
+            break;
+          default:
+            break;
+        }
+
+        return output;
+      },
+    );
+  }
+}
