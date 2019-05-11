c9ac39a047b8ebf11fecbac86080ae8984952987
Ben Yellin <byellindc@gmail.com>
2019-05-11 18:33:37 -0400

    setup project config

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+	plugins: [
+		['@babel/proposal-class-properties', {
+			loose: true
+		}],
+		['transform-for-of-as-array', {
+			loose: true
+		}],
+		['@babel/plugin-proposal-export-default-from']
+	],
+	presets: [
+		['@babel/env', {
+			corejs: 3,
+			loose: true,
+			modules: false,
+			targets: { node: 8 },
+			useBuiltIns: 'entry'
+		}]
+	]
+};
---
+++ rollup.config.js
@@ -0,0 +1,83 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const isBrowser = String(process.env.NODE_ENV).includes('browser');
+const isBrowserDev = String(process.env.NODE_ENV).includes('browserdev');
+const isCli = String(process.env.NODE_ENV).includes('cli');
+
+const pathname = isCli ? 'cli/index' : 'index';
+const input = `src/${pathname}.js`;
+const output = isBrowserDev
+	? { file: 'build/browser.js', format: 'cjs', sourcemap: true }
+: isBrowser
+	? { file: 'build/browser.min.js', format: 'cjs' }
+: isCli
+	? { file: 'build/cli.js', format: 'cjs' }
+: [
+	{ file: 'build/index.js', format: 'cjs', sourcemap: true },
+	{ file: 'build/index.mjs', format: 'esm', sourcemap: true }
+];
+const plugins = [
+	babel()
+].concat(
+	isBrowser ? [
+		nodeResolve(),
+		commonjs(),
+		babel({
+			babelrc: false,
+			presets: [
+				['@babel/env', {
+					corejs: 3,
+					loose: true,
+					modules: false,
+					targets: 'last 2 chrome versions, last 2 edge versions, last 2 firefox versions, last 2 safari versions, last 2 ios versions',
+					useBuiltIns: 'entry'
+				}]
+			]
+		}),
+		modernUMD('Abacus', 'Abacus')
+	] : [],
+	isBrowser && !isBrowserDev ? terser() : [],
+	isCli ? [
+		trimUseStrict(),
+		addHashBang()
+	] : []
+);
+
+export default { input, output, plugins };
+
+function modernUMD (name, exportName) {
+	const replacee = `module.exports = ${exportName}`;
+	const replacer = `"object"==typeof self?self.${name}=${exportName}:"object"==typeof module&&module.exports&&(module.exports=${exportName})`;
+
+	return {
+		name: 'modern-umd',
+		renderChunk (code) {
+			return `!function(){${
+				code
+				.replace(/'use strict';\n*/, '')
+				.replace(replacee, replacer)
+			}}()`;
+		}
+	};
+}
+
+function addHashBang () {
+	return {
+		name: 'add-hash-bang',
+		renderChunk (code) {
+			return `#!/usr/bin/env node\n${code}`;
+		}
+	};
+}
+
+function trimUseStrict () {
+	return {
+		name: 'trim-use-strict',
+		renderChunk (code) {
+			return code.replace(/\s*('|")?use strict\1;\s*/, '');
+		}
+	};
+}
