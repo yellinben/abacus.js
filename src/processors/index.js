ceafb1c17598b69ca6e345f2feaf07c538d54806
Ben Yellin <yellinben@gmail.com>
2019-05-28 09:04:29 -0400

    Create UnitConversionProcessor

--- src/processors/index.js
+++ src/processors/index.js
@@ -3,11 +3,13 @@ import WordMathProcessor from './word_math_processor';
 import PercentageProcessor from './percentage_processor';
 import VariableAssignmentProcessor from './variable_assignment_processor';
 import VariableExpansionProcessor from './variable_expansion_processor';
+import UnitConversionProcessor from './unit_conversion_processor';
 
 export default [
   VariableAssignmentProcessor,
   VariableExpansionProcessor,
   CommentProcessor,
   WordMathProcessor,
-  PercentageProcessor
+  PercentageProcessor,
+  UnitConversionProcessor
 ];
---
+++ src/processors/unit_conversion_processor.js
@@ -0,0 +1,23 @@
+import Processor from '../processor';
+import Unit from '../unit';
+
+export default class UnitConversionProcessor extends Processor {
+  constructor() {
+    super({
+        match: /(?<value>-?[0-9.]+)\s*(?<unit1>[a-zA-Z]+)\s*(?<operator>to|as|in|->)\s*(?<unit2>[a-zA-Z]+)/,
+        priority: 3
+      },
+      (matches) => {
+        const value = matches[1];
+
+        const unit1 = Unit.withSuffix(matches[2]);
+        const unit2 = Unit.withSuffix(matches[4]);
+
+        const result = unit1.convert(value, unit2);
+        const units = [unit1, unit2];
+
+        return {result, units};
+      },
+    );
+  }
+}
