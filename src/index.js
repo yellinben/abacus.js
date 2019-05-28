af8561c4c2658f714d4c3f2df1280510a97d02fa
Ben Yellin <yellinben@gmail.com>
2019-05-28 09:07:28 -0400

    Add Unit to global export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Context from './context';
 import Processor from './processor';
+import Unit from './unit';
 
 export default class Abacus {
   static Context = Context;
   static Processor = Processor;
+  static Unit = Unit;
 }
